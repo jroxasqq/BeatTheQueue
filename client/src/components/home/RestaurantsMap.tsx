4e3d915abb26db753ac80857fb7e597234ade76c
jroxasqq <yourName@student.unsw.edu.au>
2026-01-26 16:34:17 +1100

    Added a grid/map toogle button to the client's home page.

---
+++ client/src/components/home/RestaurantsMap.tsx
@@ -0,0 +1,5 @@
+const RestaurantsMap = () => {
+  return <div>RestaurantsMap</div>;
+};
+
+export default RestaurantsMap;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import RestaurantsGrid from "../components/home/RestaurantsGrid";
+import RestaurantsMap from "../components/home/RestaurantsMap";
 
 const HomePage = () => {
+  const [isMap, setIsMap] = useState(false);
+
   return (
     <div className="w-screen p-8">
-      <p className="mb-6 text-6xl text-black font-medium">BeatTheQueue</p>
-      <p className="mb-12 text-2xl text-gray-800">My own restaurant viewer!</p>
-      <RestaurantsGrid />
+      <div className="items-center justify-between flex flex-row">
+        <div>
+          <p className="mb-6 text-6xl text-black font-medium">BeatTheQueue</p>
+          <p className="mb-12 text-2xl text-gray-800">My own restaurant viewer!</p>
+        </div>
+        <button className="h-12" onClick={() => setIsMap(!isMap)}>
+          View as {`${isMap ? "grid" : "map"}`}
+        </button>
+      </div>
+      {isMap ? <RestaurantsMap /> : <RestaurantsGrid />}
     </div>
   );
 };
