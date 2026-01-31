4410623fd86831df804d3e577b3700bbb65a35bd
jroxasqq <yourName@student.unsw.edu.au>
2026-01-31 18:04:44 +1100

    Created a marker on the interactive map to test it.

--- client/src/components/home/InteractiveMap.tsx
+++ client/src/components/home/InteractiveMap.tsx
@@ -1,15 +1,24 @@
-"use client";
+import { useState } from "react";
+import { AdvancedMarker, APIProvider, InfoWindow, Map } from "@vis.gl/react-google-maps";
 
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
 import { config } from "../../config";
 
 const center = { lat: -33.8300330824995, lng: 151.1260496110137 };
 
 const InteractiveMap = () => {
+  const [openMarker, setOpenMarker] = useState(false);
+
   return (
     <APIProvider apiKey={config.googleMapsApiKey}>
       <div className="h-[70vh]">
-        <Map defaultZoom={11} defaultCenter={center}></Map>
+        <Map defaultZoom={11} defaultCenter={center} mapId={config.googleMapsMapId}>
+          <AdvancedMarker position={center} onClick={() => setOpenMarker(true)} />
+          {openMarker && (
+            <InfoWindow position={center} onCloseClick={() => setOpenMarker(false)}>
+              <p className="text-black">This marker has been opened!</p>
+            </InfoWindow>
+          )}
+        </Map>
       </div>
     </APIProvider>
   );
--- client/src/config.ts
+++ client/src/config.ts
@@ -2,6 +2,7 @@ type Config = {
   baseUrl: string;
   serverPort: number;
   googleMapsApiKey: string;
+  googleMapsMapId: string;
 };
 
 function getEnvOrThrow(key: string) {
@@ -14,4 +15,5 @@ export const config: Config = {
   baseUrl: `${window.location.protocol}//${window.location.hostname}`,
   serverPort: parseInt(getEnvOrThrow("VITE_SERVER_PORT")),
   googleMapsApiKey: getEnvOrThrow("VITE_GOOGLE_MAPS_API_KEY"),
+  googleMapsMapId: getEnvOrThrow("VITE_GOOGLE_MAPS_MAP_ID"),
 };
